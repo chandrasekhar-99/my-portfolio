e9d625e5fab3aaa1a0130ff7a699bf7fabcea66d
chandrasekhar <chandrasekharsathavalli99@gmail.com>
2025-06-11 14:25:10 +0530

    updated UI

--- front-end/src/components/Home/index.js
+++ front-end/src/components/Home/index.js
@@ -2,8 +2,9 @@ import './index.css'
 import Navbar from '../Navbar'
 import About from '../About'
 import Projects from '../Projects'
-import { Element } from "react-scroll";
+import { Element, Link } from "react-scroll";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Home = () => {
   return(
@@ -11,15 +12,17 @@ const Home = () => {
       <Navbar/>
         <Element name="home" className="home-container">
           <section id="home" className="home-section">
-            <p className="home-description">Hi! I'm</p>
-            <h1 className="home-heading"> Chandra Sekhar Sathavalli</h1>
-            <p className="home-description">Aspiring full-stack developer specializing in the <span className="span-highlight">MERN stack.</span></p>
-            <div >
-              <FaGithub className="home-icons"/>
-              <FaLinkedin className="home-icons"/>
-            </div>
-            <div className="section"></div>
+              <p className="home-description">Hi! I'm</p>
+              <h1 className="home-heading"> Chandra Sekhar Sathavalli</h1>
+              <p className="home-description">Aspiring full-stack developer specializing in the <span className="span-highlight">MERN stack.</span></p>
+              <div >
+                <FaGithub className="home-icons"/>
+                <FaLinkedin className="home-icons"/>
+              </div>
           </section>
+          <Link to="about" spy={true} offset={-90} smooth={true} duration={600} className="home-scroll-link">
+           <MdKeyboardArrowDown className="home-scroll-icon" />
+          </Link>
         </Element>
       <About/>
       <Projects/>
