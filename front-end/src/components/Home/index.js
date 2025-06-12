e486fb1e11d4db8502577c5f4d697e4fb364131d
chandrasekhar <chandrasekharsathavalli99@gmail.com>
2025-06-12 13:53:07 +0530

    ui updated

--- front-end/src/components/Home/index.js
+++ front-end/src/components/Home/index.js
@@ -14,7 +14,7 @@ const Home = () => {
           <section id="home" className="home-section">
               <p className="home-description">Hi! I'm</p>
               <h1 className="home-heading"> Chandra Sekhar Sathavalli</h1>
-              <p className="home-description">Aspiring full-stack developer specializing in the <span className="span-highlight">MERN stack.</span></p>
+              <p className="home-description-2">Aspiring full-stack developer specializing in the <span className="span-highlight">MERN stack</span>, with a passion for building creative, user-friendly applications. Actively learning and expanding my technical skills.</p>
               <div >
                 <FaGithub className="home-icons"/>
                 <FaLinkedin className="home-icons"/>
